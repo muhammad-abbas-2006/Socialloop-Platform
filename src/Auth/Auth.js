7ecec2dd052868784776650d6a8a9ee319253b4d
Muhammad Abbas <muhammadabuabbas907@gmail.com>
2026-03-07 00:47:18 +0500

    Update Auth.js

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,9 +1,4 @@
-// import { createClient } from '@supabase/supabase-js'
-// const supabaseUrl = process.env.SUPABASE_URL
-// const supabaseKey = process.env.SUPABASE_KEY
-// export const supabase = createClient(supabaseUrl, supabaseKey)
-
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://gqoajzzpjtmljquwreeu.supabase.co'
-const supabaseKey = 'sb_publishable_4PJ7LmyvnoIQSxh8vYMcGw_z4Ea5tts'
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
